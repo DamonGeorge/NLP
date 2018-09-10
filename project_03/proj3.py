a5039c7abb65b9a103f88e71e2e5a9587ca43afd
Damon George <33606025+DamonGeorge@users.noreply.github.com>
2018-09-10 16:37:47 -0700

    updated to new project specs

--- project_03/proj3.py
+++ project_03/proj3.py
@@ -3,7 +3,7 @@ Team Member #1: Robert Brajcich
 Team Member #2: Damon George
 Zagmail address for team member 1: rbrajcich@zagmail.gonzaga.edu
 Zagmail address for team member 2: dgeorge2@zagmail.gonzaga.edu
-Project 3: Extracts tokenized inaugural addresses into files and
+Project 3: Extracts tokenized inaugural addresses into a serialized file and
 	finds the frequency of a word in each address
 Due: 9/14/2018
 '''
@@ -12,92 +12,95 @@ Due: 9/14/2018
 from nltk.corpus import inaugural
 import matplotlib.pyplot as plt
 import re
+import pickle
 
-
-def write_address_file(address):
+def read_address(address):
 	'''
-	Writes all of the provided inaugural address to a new file.
+	Reads the given nltk inaugural address to a string
 	'''
+	full_address = ""
 
-	#create new txt file
-	with open(address, 'w') as fout:
-		#join all the words in each  sentence 
-		for sent in inaugural.sents(address):
-			sent = ' '.join(sent)
-			fout.write(sent + '\n')
+	#join all the words in each  sentence 
+	for sent in inaugural.sents(address):
+		sent = ' '.join(sent)
+		full_address = full_address + sent + '\n'
 
-def tokenize_address(address):
+	return full_address
+	
+
+def tokenize(string):
 	'''
-	Reads the specified address from its txt file and 
-	returns a new string that is the address converted to lower case
-	with all non-word characters removed. Spaces are not removed. 
+	Returns a list of words parsed from the string with all non-word characters removed.
 	'''
-
-	#read the address and remove new lines
-	with open(address, 'r') as fin:
-		string = re.sub('\n',' ', fin.read())
-		
 	#lower and substitute all non-word characters with spaces
 	string = string.lower()
 	new_str = re.sub(r'[^a-z ]', ' ', string)
-	return new_str
+
+	words = new_str.split()
+
+	return words
 
 
 def main():
-	'''
-	Gets a word from the user. Writes out all inaugural addresses from nltk to their own files.
-	Then this program tokenizes all addresses into a singular file.
-	From this file, the program counts the number of instances of the given word,
-	and plots this count over each year of inaugurual addresses. 
-	'''
+
+	#Part 1: load inaugural addresses, tokenize, and serialize to pickle file
+	#=============================================
+	
+	#get files names from nltk library
+	file_ids = inaugural.fileids()
+	
+	#list to all hold tokenized addresses
+	tokenized_addresses = []
+
+	#lists to hold years and the frequency counts to use in the graph
+	years = []
+	counts = []
+
+	#loop through all inaugural addresses
+	for address in file_ids:
+		#add year to list - the year is the first 4 characters of each address title
+		years.append(int(address[:4]))
+
+		#read the address into a string of newline separated sentences
+		string = read_address(address)
+
+		#tokenize each address into a list of lowercase words
+		words = tokenize(string)
+
+		#append the tokenized address to the master list
+		tokenized_addresses.append(words)
+
+	#serialize list of addresses to pickle file
+	with open('proj3.pkl', 'wb') as fout:
+		pickle.dump(tokenized_addresses, fout)
+	
+	#Part 2: Load pickle data, search word, and plot frequency
+	#=============================================
 
 	#get the word to search
 	search = input("Enter word to search: ")
 	search.strip('"\'') # remove quotes if grader tries to use them
 	search.lower();		# lower since tokenizing lowers all letters in the addresses
 
-	#get files names from nltk library
-	file_ids = inaugural.fileids()
-	
-	#lists to hold years and the frequency counts to use in the graph
-	years = []
-	counts = []
+	with open('proj3.pkl', 'rb') as fin:
+		parsed_addresses = pickle.load(fin)
 
-	#create output file
-	with open('proj3.txt', 'w') as fout:
-		#loop through all inaugural addresses
-		for address in file_ids:
-			#add year to list - the year is the first 4 characters of each address title
-			years.append(int(address[:4]))
 
-			#write each address to file
-			write_address_file(address)
+	#loop through each address
+	for address in parsed_addresses:
+		#initial frequency is 0
+		count = 0
 
-			#tokenize each address, append a newline to separate addresses, and write to output
-			string = tokenize_address(address)
-			string = string + '\n'
-			fout.write(string)
-	
-	#open output file 
-	with open('proj3.txt', 'r') as fin:
-		#loop through each address
-		for address in fin:
-			#split into words 
-			words = address.split();
+		#Increment the count if each word matches the input
+		for word in address:
+			if word == search:
+				count = count + 1
 
-			#initial frequency is 0
-			count = 0
+		#append count to the list 
+		counts.append(count)
 
-			#Increment the count if each word matches the input
-			for word in words:
-				if word == search:
-					count = count + 1
-
-			#append count to the list 
-			counts.append(count)
-
-	#plot the word count vs year
-	plt.plot(years,counts)
+	#plot the year vs word count
+	plt.plot(counts,years)
 	plt.show()
 
 
