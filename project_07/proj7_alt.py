90e1b36dcd251bedfd161417bd58a3340827ecf6
Damon George <33606025+DamonGeorge@users.noreply.github.com>
2018-11-01 21:05:14 -0700

    more edits to project 7

--- project_07/proj7_alt.py
+++ project_07/proj7_alt.py
@@ -17,14 +17,17 @@ from collections import OrderedDict
 import random
 import re
 
-def normalize_token(token):
+START_TAG = '<s>'
+END_TAG = '</s>'
+
+def normalize_line(line):
 	'''
-	Takes a single token, converts it to lowercase, and remove
+	Takes a string, converts it to lowercase, and removes
 	any non-alphabetic characters except quotes
 	'''
-	token = token.lower()
-	token = re.sub(r'[^a-z\']', '', token)
-	return token
+	line = line.lower()
+	line = re.sub(r'[^a-z\' ]', '', line)
+	return line
 
 def keep_token(token):
 	'''
@@ -32,7 +35,7 @@ def keep_token(token):
 	otherwise true
 	'''
 	# since bad chars have already been removed, just check if empty
-	return token != '' 
+	return bool(token)
 
 def parse_tokens():
 	tokens = [] # holds all words
@@ -41,33 +44,42 @@ def parse_tokens():
 	# open file and loop through each line
 	with open('100-0.txt', 'r') as fin:
 		for line in fin:
-			tokens.append('<s>') # start of line
-			sentence = [normalize_token(i) for i in line.split(' ')] # get tokens and normalize
-			for word in sentence:
-				if keep_token(word): # append words to list if not empty
-					tokens.append(word) 
-			tokens.append('</s>') # end of line
+			sentence = normalize_line(line) # remove bad characters
+			sentence = [i for i in sentence.split(' ') if keep_token(i)] # get tokens and remove bad ones
+
+			if len(sentence) > 0 : # if sentence is not empty
+				tokens.append(START_TAG) # start of line
+				tokens.extend(sentence) # tokens
+				tokens.append(END_TAG) # end of line
 
 	return tokens
 
 def count_ngrams(all_words, N):
 	'''
 	Counts ngrams in the all_words list of words. 
-	Returns dictionary in the form (ngram_tuple : count)
+	Returns dictionary in the form (ngram_tuple : count) and total count of ngrams
 	'''
 	ngram_counts = {} 
+	total_count = 0
 
 	# make sure min length is met
 	if len(all_words) >= N:
-		for i in range(len(all_words) - (N-1)): # loop through all words
+		for i in range(len(all_words) - (N-1)):
 			ngram = tuple(all_words[i:i+N]) # get current ngram
 
-			if ngram in ngram_counts: 
-				ngram_counts[ngram] += 1
-			else:
-				ngram_counts[ngram] = 1
+			# only record tags that don't have sentence markers in the middle
+			if START_TAG not in ngram[1:] \
+				and END_TAG not in ngram[:-1] \
+				and not (START_TAG == ngram[0] and END_TAG == ngram[N-1]):
 
-	return ngram_counts
+				# increment count and update count in dict
+				total_count += 1 
+				if ngram in ngram_counts: 
+					ngram_counts[ngram] += 1
+				else:
+					ngram_counts[ngram] = 1
+
+	return ngram_counts, total_count
 
 def calc_ngram_frequencies(ngram_counts, total_ngrams):
 	'''
@@ -104,90 +116,104 @@ def generate_sentence(frequencies, N, show_tags, num_ngrams):
 	sentence with a period. Includes sentence markers if show_tags is true.
 	'''
 	sentence = ""
-	
-	# add start sentence marker if needed
-	if show_tags: 
-		sentence += "<s> "
 
 	# generate first ngram
 	first_ngram = generate_ngram(frequencies)
 
 	if N == 1: # for unigrams
-
 		# loop through unigrams to generate
 		for i in range(num_ngrams):
 			# generate next unigram
 			ngram = generate_ngram(frequencies)
 			
 			# loop until ngram doesn't include sentence marker
-			while ('<s>' in ngram) or ('</s>' in ngram):
+			while (START_TAG in ngram) or (END_TAG in ngram):
 				ngram = generate_ngram(frequencies)
 
 			# add ngram to sentence
 			sentence += ' ' + ' '.join(ngram)
 	
 	elif N > 1: # for ngrams, not unigrams
-
-		# loop until we have a start sentence marker and NO other sentence markers
-		while (first_ngram[0] != '<s>') or ('<s>' in first_ngram[1:]) or ('</s>' in first_ngram[1:]):
+		# loop until we have a start sentence marker
+		while first_ngram[0] != START_TAG:
 			first_ngram = generate_ngram(frequencies)
 
 		# add first ngram and capitalize
-		sentence += ' ' + ' '.join(first_ngram[1:]).capitalize()
+		sentence += ' ' + ' '.join(first_ngram[1:])
 
-		# 
+		# add ngrams in the middle of the sentence
 		for i in range(num_ngrams - 2):
 			ngram = generate_ngram(frequencies)
-			while ('<s>' in ngram) or ('</s>' in ngram):
+
+			# loop until ngram has no sentence markers, then add to sentence
+			while (START_TAG in ngram) or (END_TAG in ngram):
 				ngram = generate_ngram(frequencies)
+			
 			sentence += ' ' + ' '.join(ngram)
 		
+		# last ngram
 		last_ngram = generate_ngram(frequencies)
 
-		while (last_ngram[N-1] != '</s>') or ('<s>' in last_ngram[:-1]) or ('</s>' in last_ngram[:-1]):
+		# loop until we have end sentence marker and add to sentence
+		while last_ngram[N-1] != END_TAG:
 			last_ngram = generate_ngram(frequencies)
 
 		sentence += ' ' + ' '.join(last_ngram[0:N-1])
 
+	# start and end to string
 	start = ''
 	end = '.'
 	if show_tags:
-		start += '<s> '
-		end += ' </s>'
+		start += START_TAG + ' '
+		end += ' ' + END_TAG
 		
 	return start + sentence[1:].capitalize() + end
 
 def get_sentence_marker_preference():
+	'''
+	Get user's preference as to whether to display sentence markers.
+	Returns boolean value
+	'''
 	# Prompt for yes or no 
 	user_input = raw_input("Display Sentence Markers (y/n): ")
 	checking_input = True
+	
 	# loop until valid input is provided
 	while checking_input:
-		val = user_input.strip('"\'').lower()
-		if val == 'y':
-			display_sentence_markers = True
-			checking_input = False
-		elif val == 'n':
-			display_sentence_markers = False
-			checking_input = False
-		else:
+		val = user_input.strip('"\'').lower() # get and clean input
+		
+		# if invalid input try again, otherwise return choice
+		if val != 'y' and val != 'n': 
 			print("Invalid input")
 			user_input = raw_input("Display Sentence Markers (y/n): ")
 			checking_input = True
+		else:
+			display_sentence_markers = (val == 'y')
+			checking_input = False
 
 	return display_sentence_markers
 
 def main():
+	# get user's show marker preference
 	show_sentence_markers = get_sentence_marker_preference()
 
 	# get lowercase list of all words
 	tokens = parse_tokens()
 
 	print("\nN-grams: ")
+
+	# loop through unigrams to quadgrams
 	for i in range(1,5):
+		# print ngram number
 		print("\nN = " + str(i));
-		ngram_counts = count_ngrams(tokens, i)
-		ngram_freqs = calc_ngram_frequencies(ngram_counts, len(tokens) - (i-1))
+
+		# count the ngrams
+		ngram_counts, total_count = count_ngrams(tokens, i)
+
+		# calc the frequencies
+		ngram_freqs = calc_ngram_frequencies(ngram_counts, total_count)
+
+		# generate 5 line
 		for j in range(5):
 			print(generate_sentence(ngram_freqs, i, show_sentence_markers, (int) (12/i)))
 
